e856040c55f6f2c74b4a911cef4a934f7732d90b
Suhaib Osmani <osuhaibos@gmail.com>
2025-07-24 02:31:57 +0500

    //First Push

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Welcome to Tic-Tac-Toe!");
+string move;
+string[] positions = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+string board = $" {positions[0]} | {positions[1]} | {positions[2]} \n___|___|___\n {positions[3]} | {positions[4]} | {positions[5]} \n___|___|___\n {positions[6]} | {positions[7]} | {positions[8]} \n   |   |   ";
+Console.WriteLine(board);
+
+do
+{
+    Console.WriteLine("Choose your move: 1~9");
+    move = Console.ReadLine();
+}
+while (Convert.ToInt32(move) < 1 || Convert.ToInt32(move) > 9);
+//Make an array for positions, and have the user enter the index to place their mark

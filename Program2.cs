33f9f25ad858040b73c5e34d375a3c3deee5a974
Suhaib Osmani <osuhaibos@gmail.com>
2025-08-07 04:57:55 +0500

    Fifth

--- Program2.cs
+++ Program2.cs
@@ -1,7 +1,36 @@
-class Program2
+public class Program2
 {
-    static void Main()
+    public static void Main()
     {
-        Console.Write("User 1, enter a number between 0 and 100: ");
+        int answer;
+        int guess;
+        bool rightGuess = false;
+
+        do
+        {
+            Console.Write("User 1, enter a number between 0 and 100: ");
+        }
+        while (!int.TryParse(Console.ReadLine(), out answer) || answer > 100 || answer < 0);
+        Console.Clear();
+
+        Console.WriteLine("User 2, guess the number");
+        do
+        {
+            Console.Write("What is your next guess? ");
+            int.TryParse(Console.ReadLine(), out guess);
+            if (guess - answer > 0)
+                Console.WriteLine($"{guess} is too big.");
+            else if (guess - answer < 0)
+                Console.WriteLine($"{guess} is too small.");
+            else if (guess - answer == 0)
+            {
+                Console.WriteLine("You guessed the number!");
+                rightGuess = true;
+                break;
+            }
+
+        }
+        while (rightGuess != false);
+
     }
 }
